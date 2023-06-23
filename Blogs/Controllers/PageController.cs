022d38ce4419f6bcdd17166612b57a84b7cba565
Sirish22 <sirishshrestha22@gmail.com>
2023-06-23 15:10:03 +0545

    BLog

---
+++ Blogs/Controllers/PageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogs.Controllers
+{
+    public class PageController : Controller
+    {
+       public IActionResult About()
+        {
+            return View();
+        }
+        
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Blogs/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Hosting;
+
+namespace Blogs.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        //relation 
+        public List<Post>? Post { get; set; }
+    }
+}
